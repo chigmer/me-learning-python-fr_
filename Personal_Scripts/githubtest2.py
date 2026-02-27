2dde47862f76e849d2c15fb7d78db9cf342b7808
chigmerexit <263529761+chigmer@users.noreply.github.com>
2026-02-27 22:54:07 +0800

    testing git rn

---
+++ Personal_Scripts/githubtest2.py
@@ -0,0 +1,10 @@
+
+# If Git doesn't detect this file after I save it,
+# then something is very wrong.
+
+greetings = ["Hello", "Hi"]
+names = ["GitHub", "buHtiG"]
+
+for greeting in greetings:
+    for name in names:
+        print(f"{greeting} {name}")
