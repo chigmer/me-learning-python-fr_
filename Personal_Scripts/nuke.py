3ae41bfa56c47d20b92f433c078c9890d2a90364
chigmerexit <263529761+chigmer@users.noreply.github.com>
2026-03-08 13:28:18 +0800

    add comments on recent script

--- Personal_Scripts/nuke.py
+++ Personal_Scripts/nuke.py
@@ -1,12 +1,17 @@
 def nuclear_bomb():
     a = []
+    """
+    Memory allocation stress test.
+    
+    This will crash Python by filling RAM.
+    OS will kill the process when memory is exhausted.
+    
+    Educational experiment - advisable to NOT run on shared systems
+    """
     while True:
         a.append([0] * 100000000)  
         #a lotta zeroes
-#nuclear_bomb()
-       
         
-
-
-            
-           
+        
+#nuclear_bomb() #commented out for safety (you're welcome)
+                        
