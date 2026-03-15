214e346c561b37723ab0b9ac4210b77f46253cbe
chigmerexit <263529761+chigmer@users.noreply.github.com>
2026-03-15 21:45:49 +0800

    Arrived on the introductions of Chapter 10

---
+++ Personal_Scripts/pathtest.py
@@ -0,0 +1,9 @@
+import os
+from pathlib import Path
+
+print(os.path.abspath(__file__))
+print(os.getcwd())
+print("\n\n\n")
+p = Path("something")
+print(str(p) + "\n\n\n")
+print(Path("bacon", "eggs", "sushi"))
