2151a3486f84589ac168d3749adeaa71136d60d5
chigmerexit <263529761+chigmer@users.noreply.github.com>
2026-03-30 21:32:20 +0800

    Modify renumber_file.py to include functions that organize files

--- ATBSWP_book/renumber_files/renumber_file.py
+++ ATBSWP_book/renumber_files/renumber_file.py
@@ -10,21 +10,61 @@ def find_files(path=Path.cwd())->list:
     if not isinstance(path,Path):
         raise ValueError("expected Path obj")
     elif not path.is_dir():
-        raise ValueError("expected directory.")
-    files = []
-    for filepath in path.rglob("*.*"):
-        files.append(filepath.name)
-        
+        raise ValueError("expected directory.")  
     pattern = re.compile(r"""
-    ([\w-]+)
-    (\d+)
-    \.
+    ([a-zA-Z]+)
+    (\d+)\.    
     ([a-zA-Z]+)
     
-    """,re.VERBOSE)
+    """,re.VERBOSE) 
+    file_info = []
+         
+    for filepath in path.iterdir():
+        match = re.fullmatch(pattern,filepath.name)
+        if match:
+            file_info.append((match.group(1),int(match.group(2)),match.group(3),Path(filepath.name)))
+    return file_info
+        
+        
+ 
+#newer update
+#i got the list of tuples working .   
+#breakdown before i go, it returns (prefix,number,extension,Path obj to file)   
+   
+   
+    #older update
     #todo: organize all files with a common prefix and filename extension
     #then be able to identify which stuff is missing using the function below.
-    #bye
-def find_ordering(files):
-    pass
+    
+    
+def find_ordering(files:tuple):
+    #everything should be in a dictionary.
+    #no need to do any dev checks or stuff. this is just for the user, if it breaks, its not my fault.
+    file_collection = {}
+    for file in files:
+        key = (file[0], file[2])
+        file_collection.setdefault(key, []).append(file[1])
+    return file_collection
+    
+def find_gaps(collection:dict):
+    for entry in collection:
+        imperfect_set = set(collection[entry]) #list obj turned to a set               
+        perfect_set = set(range(1,max(imperfect_set) + 1))
+        #unfinished
+        
+        return perfect_set - imperfect_set
+        
+        
+            #the values within entry
+    
+print(find_ordering(find_files()))    
+print("yo")
+
+#missing files:
+missing = find_gaps(find_ordering(find_files()))
+    
+      
+    
+    
+    
     
