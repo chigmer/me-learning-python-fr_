c79342a45ed41f4d17ea02258b04102aaee949da
chigmerexit <263529761+chigmer@users.noreply.github.com>
2026-05-19 12:06:19 +0800

    Completion: chapter 18, skipping the practice program since the json one is almost similar to it

---
+++ ATBSWP_book/chapter_018.py
@@ -0,0 +1,37 @@
+"""Practice Questions
+
+1. What are some features that Excel spreadsheets have but CSV spread-sheets don’t?
+
+2. What do you pass to csv.reader() and csv.writer() to create reader and writer objects?
+
+3. What modes do File objects for reader and writer objects need to be opened in?
+
+4. What method takes a list argument and writes it to a CSV ﬁle? 
+
+5. What do the delimiter and lineterminator keyword arguments do? 
+
+6. Of CSV, JSON, and XML, which formats can be easily edited with a text editor application?
+
+7. What function takes a string of JSON data and returns a Python data structure?
+
+8. What function takes a Python data structure and returns a string of JSON data?
+
+9. Which data serialization format resembles HTML, with tags enclosed in angle brackets?
+
+10. How does JSON write None values? 
+
+11. How do you write Boolean values in JSON?"""
+
+"""
+1. text formatting, formulas and conversion 
+2. file objects from open()
+3. read mode for reader, every writing mode for writer is valid (i think)
+4. csv.writer()
+5. delimiter is the value separator, line terminator is the value after each line (almost always a newline)
+6. JSON and CSV
+7. json.load/s()
+8. json.dump/s() (both are either for files or strings)
+9. XML
+10. null
+11. true / false without capital letters
+"""
