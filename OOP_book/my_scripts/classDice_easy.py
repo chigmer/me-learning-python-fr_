e57f0d420600fe0a271a0d867df575c67858f9d4
chigmerexit <263529761+chigmer@users.noreply.github.com>
2026-06-09 21:06:25 +0800

    easy practice class, still reading theory :/

---
+++ OOP_book/my_scripts/classDice_easy.py
@@ -0,0 +1,12 @@
+import random
+
+class Dice():
+    def __init__(self):
+        self.faces = list(range(1,7))
+    def roll(self):
+        #methods shouldnt 'print' stuff, just return a value, right?
+        value = random.choice(self.faces)
+        print(f"You rolled:\n\n{value}")
+if __name__ == "__main__":
+    dice = Dice()
+    dice.roll()
