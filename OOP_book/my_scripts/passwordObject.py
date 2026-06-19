267aacf9e89d690035acabf7bb5687b9d541297c
chigmerexit <263529761+chigmer@users.noreply.github.com>
2026-06-19 09:38:37 +0800

    password class. Reusable, configurable, cryptographically secure, validates inputs, handles edge cases

--- OOP_book/my_scripts/passwordObject.py
+++ OOP_book/my_scripts/passwordObject.py
@@ -16,7 +16,7 @@ class Password:
 
 'numbers' : "0123456789",
 
-'special_characters' : " !?.,:;-_()[]{}'\"/@#&%+*=<>"
+'special_characters' : "_$&@;!?.,"
 } 
        
 
@@ -38,12 +38,12 @@ class Password:
             special_chars
         )
         if exclusions is None:
-            exclusions = []
+            exclusions = []        
         char_bank = self.char_bank.copy()
         if not letters:
             del char_bank["uppercase"]
             del char_bank["lowercase"]
-        elif letters:
+        else:
             if not uppercase:
                 del char_bank["uppercase"]
             if not lowercase:
@@ -59,8 +59,9 @@ class Password:
          
         else:
             chars = ""
-            for i in char_bank.values():
-                chars += i
+            for v in char_bank.values():
+                chars += v
+        self.chars = chars
         if not isinstance(length, int):
             raise TypeError("length must be an int")
 
@@ -70,31 +71,84 @@ class Password:
         if length >= 100000:
             raise ValueError("dont make a password this long.")
         if exclusions: 
-            char_bank = list(char_bank)
-        for ex in exclusions:
+            chars = self._apply_exclusions(exclusions)
             
-            if ex in char_bank:
-                char_bank = [character for character in char_bank if character != ex]
-        if len(char_bank) < 1:
+        if len(chars) < 1:
             return 
-            
-        char_bank = ''.join(char_bank)                       
+                                        
         password = ""                                 
 
         for _ in range(length):
-            password += secrets.choice(char_bank)
-
+            password += secrets.choice(chars)        
+        return password
+        
+        
+    def _apply_exclusions(self,exclusions:str|list):
+        try:
+            bank = list(self.chars)
+            if isinstance(exclusions,(str)):
+                exclusion_unique = list(set(exclusions))
+                
+                
+                
+            elif isinstance(exclusions,(list,tuple)):
+                #i dont know what to do if a list has a two or more long string as a value
+                exclusion_unique = []
+                for i in exclusions:
+                    if isinstance(i,(str,int)):
+                        exclusion_unique += list(i if isinstance(i,str) else str(i))
+                    
+                    else:
+                        raise TypeError("expected str or int in exclusions param")
+                               
+                        
+                            
+                exclusion_unique = list(set(exclusion_unique))    
+                
+            return [x for x in bank if x not in exclusion_unique]
+        except TypeError as e:
+            raise TypeError(f"error: {e}")
+            #ValueError? or Type Error? or something else??
+         
+                        
+                        
+            
+        
+                     
+                
+                
+                
+        
     def _validate_param(self,*args):
         for arg in args:
             if not isinstance(arg, bool):
-                raise TypeError("invalid type for parameter")
+                raise TypeError(f"expected bool, got {type(arg).__name__}")
 
 
 if __name__ == "__main__":
     oPassword = Password()
-    pass_1 = oPassword.create_password(length=20)
-    pass_2 = oPassword.create_password(length=20,letters=False)
-    print(f"PASS 1: {pass_1}\nPASS 2: {pass_2}")
+    #i just want to say, this is horrible, i know its very inefficient, but i lack the mind to catch and fix it
+    #exclusion logic is actually harder than it looks
+    pass_1 = oPassword.create_password(length=20,exclusions=list(range(1,10)))
+    pass_2 = oPassword.create_password(length=20,letters=False,special_chars=False,exclusions="123456789")
+    pass_3 = oPassword.create_password(length=25,special_chars=False)
+    print(f"PASS 1(must have no numbers): {pass_1}\nPASS 2(should be 20 zeroes): {pass_2}\nPASS 3(only letters and numbers): {pass_3}")
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
 
 """
@@ -126,7 +180,7 @@ while True:
         continue
 
     if length > 60:
-        print("too long for a standard password. you are NOT protecting the US government.")
+        print("too long for a standard password get outta here.")
     elif length <= 3:
         print("invalid length")
 
