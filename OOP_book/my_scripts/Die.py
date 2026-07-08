b939b5aa25a76d39316e7df935de5302e56b17bd
chigmerexit <263529761+chigmer@users.noreply.github.com>
2026-07-08 15:01:20 +0800

    add: stats method for Die, + more attributes

--- OOP_book/my_scripts/Die.py
+++ OOP_book/my_scripts/Die.py
@@ -6,19 +6,33 @@ class Die:
         elif num_of_sides <= 0:
             raise ValueError
         self.sides = num_of_sides
-        self.results = []
+        self.results = [] 
+        self.frequencies = {}
+        self.avg_num = 0
+        for i in range(self.sides):
+            self.frequencies[i+1] = 0
+              
     def roll(self):
         result = random.randint(1,self.sides)
         self.results.append(result)
         return result
-    def avg_num(self):
+    def calculate_stats(self):
         if self.results:
-            return round(sum(self.results) / len(self.results),3)
+            self.avg_num = round(sum(self.results) / len(self.results),3)
+            
+            for i in self.results:
+                
+                self.frequencies[i] += 1
 if __name__ == "__main__":
     die = Die()
-    for _ in range(1002):
-        print(die.roll())
-    print(f"\nresults:\n\n{die.results}")
-    print("Avg")
-    print(die.avg_num())
+    for _ in range(25):
+        die.roll()    
+    print("Current stats! (no updating)")
+    print(f"sides: {die.sides}\nresults: {die.results}\nfrequencies: {die.frequencies}\navg: {die.avg_num}")
+    print("After: ")
+    die.calculate_stats()
+    print(f"sides: {die.sides}\nresults: {die.results}\nfrequencies: {die.frequencies}\navg: {die.avg_num}")
+    
+    
+    
        
